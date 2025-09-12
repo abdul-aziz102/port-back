3a761fd0758b07cf64ba611bc27f04e7b6b3a247
Abdul Rehman <abdul@example.com>
2025-09-12 04:50:27 -0700

    g

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import createdb from './db.js';   // ✅ db.js import
+import express from "express";
+import cors from "cors";
+import createdb from "./db.js"; // ✅ db.js import
 
-import ContactRoute from './routes/contact_route.js';
+import ContactRoute from "./routes/contact_route.js";
 
 const app = express();
 
 
-app.use(cors({
-  origin: ["https://ab-aziz-port.vercel.app"],
-  methods: ["GET", "POST"],
-}));
+app.use(
+  cors({
+    origin: ["https://ab-aziz-port.vercel.app"],
+    methods: ["GET", "POST"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/contact', ContactRoute);
+app.use("/contact", ContactRoute);
 
-app.get('/', (req, res) => {
-  res.send("Hello from Express");
+app.get("/", (req, res) => {
+  res.send("Hello from Express + Vercel ✅");
 });
 
-export default app;   // Vercel ke liye export
+// ✅ Important for Vercel
+export default app;
